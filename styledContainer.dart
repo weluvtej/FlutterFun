7537ea9ef01df2cd703a83919aca0e253664f972
groslaille <56320226+groslaille@users.noreply.github.com>
2021-10-27 14:57:16 -0400

    Update styledContainer.dart
    Added text and a padding to your container if that's the style you were going for

--- styledContainer.dart
+++ styledContainer.dart
@@ -1,8 +1,15 @@
 Center(
   child: Container(
     margin: const EdgeInsets.all(10.0),
+    padding: const EdgeInsets.all(10.0),
+    width: 2.0,
     color: Colors.amber[600],
     width: 48.0,
     height: 48.0,
+    child: Text(
+      "Hi I am a container",
+      fontSize:30,
+      color:Colors.red[500],
+    )
   ),
 )

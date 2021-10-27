ffb14243a64e6b03be6e63b40514c92f522bcd6c
groslaille <56320226+groslaille@users.noreply.github.com>
2021-10-27 14:29:18 -0400

    Update styledContainer.dart
    Changed the color and added a text

--- styledContainer.dart
+++ styledContainer.dart
@@ -1,8 +1,9 @@
 Center(
   child: Container(
     margin: const EdgeInsets.all(10.0),
-    color: Colors.amber[600],
+    color: Colors.red[500],
     width: 48.0,
     height: 48.0,
+    child:Text("Hello there!")
   ),
 )
